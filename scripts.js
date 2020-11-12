150f3366df54416ac97683804a381c5cc363b4af
polearm123 <shaggywaggy29@gmail.com>
2020-11-12 10:48:31 +0000

    added a read toggle button event on each book card. functionalised and commented code for clarity

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 let myLibrary = [];
 
 function Book(title,author,pages,read){
@@ -52,25 +53,45 @@ function createBookCard(book){ //creates a card with necessary book information 
  
 }
 
-function removeBook(event){
+function removeBook(event){//removes a book of a specific
+    
+    let currentBooks = getCurrentBooks();
+    currentBooks.splice(event.target.id,1);
+    localStorage.setItem("library",JSON.stringify(currentBooks));
 
-    console.log("inside remove book function");
+    displayBooks();
+    
+}
 
-    myLibrary.splice(event.target.id,1);
+function submitNewBook(){ //adds a new book to the local storage after retrieving user input
 
+    var title = document.getElementById("book-title").value;
+    var author = document.getElementById("book-author").value;
+    var pages = document.getElementById("book-pages").value;
+    var read = document.getElementById("book-read").value;
+    var userBook = new Book(title,author, parseInt(pages), read);
+    
+    let currentBooks = getCurrentBooks();
+    currentBooks.push(userBook);
+    setCurrentBooks(currentBooks);
+    
+    document.querySelector(".add-new-book").innerHTML = ""; //removes the form from user sight
     displayBooks();
     
 
 
 }
 
+
+
 function newBookButtonForm(event){//creates a form that adds a book to the list
 
-    
 
     let formContainer = document.querySelector(".form-container");
-    let form = document.createElement("form");
+    let form = document.createElement("div");
+    form.classList.add("add-new-book");
 
+   
     let titleInput = document.createElement("input");
     titleInput.id = "book-title";
     titleInput.placeholder = "Title: ";
@@ -87,8 +108,9 @@ function newBookButtonForm(event){//creates a form that adds a book to the list
     read.id = "book-read";
     read.placeholder= "have you read this book?";
 
-    let submitBookButton = document.createElement("input");
-    submitBookButton.type = "submit";
+    let submitBookButton = document.createElement("button");
+    submitBookButton.id = "add-book";
+    submitBookButton.textContent = "Add Book";
 
     form.appendChild(titleInput);
     form.appendChild(authorInput);
@@ -98,21 +120,47 @@ function newBookButtonForm(event){//creates a form that adds a book to the list
 
     formContainer.appendChild(form);
 
-    submitBookButton.addEventListener("submit",() => {
-
-        var title = document.getElementById("book-title");
-        var author = document.getElementById("book-author");
-        var pages = document.getElementById("book-pages");
-        var read = document.getElementById("book-read");
-         
-        var userBook = new Book(`${title}`,`${author}`, parseInt(pages), `${read}`);
-        myLibrary.push(userBook);
-
-    })
-
-  
+    submitBookButton.addEventListener("click",submitNewBook);
 
     
+}
+
+function getCurrentBooks(){
+
+    return JSON.parse(localStorage.getItem("library"));
+
+}
+
+
+
+function setCurrentBooks(changedBookArray){
+    
+    localStorage.setItem("library", JSON.stringify(changedBookArray));
+
+
+}
+
+function readBook(e){
+
+    let bookToToggleRead = document.getElementById(e.target.id).id.toString();
+    let indexToToggleRead  = bookToToggleRead.slice(4,);
+
+    currentBooks = getCurrentBooks();
+
+    if(currentBooks[indexToToggleRead].read == true){
+
+        currentBooks[indexToToggleRead].read = false;
+        console.log(currentBooks[indexToToggleRead].read)
+
+    }else{
+
+        currentBooks[indexToToggleRead].read = true;
+
+    }
+
+    setCurrentBooks(currentBooks);
+
+    displayBooks();
 
 }
 
@@ -120,23 +168,29 @@ function displayBooks(){ //iterates through all books, creates a card to each an
 
     let libraryContainer = document.querySelector(".library-container");
     libraryContainer.innerHTML = "";
+    let currentBooks = getCurrentBooks();
     
     let bookInformationContainer;
     let bookCard;
 
-    console.log("inside function");
-    for(let i=0;i<myLibrary.length;i++){
+    for(let i=0;i<currentBooks.length;i++){
 
       
-        bookCard = createBookCard(myLibrary[i]);
-       
+        bookCard = createBookCard(currentBooks[i]);
 
         let removeButton = document.createElement("button");
-        removeButton.id = `${i}`;
+        removeButton.id = `remove${i}`;
         removeButton.textContent = "remove";
+
+        let readButton = document.createElement("button");
+        readButton.id = `read${i}`;
+        readButton.textContent = "read";
     
         removeButton.addEventListener("click", removeBook);
+        readButton.addEventListener("click", readBook);
+
         bookCard.appendChild(removeButton);
+        bookCard.appendChild(readButton);
 
         libraryContainer.appendChild(bookCard);
                 
@@ -144,13 +198,19 @@ function displayBooks(){ //iterates through all books, creates a card to each an
 
 }
 
-let book1 = new Book("Harry Potter: Goblet of Fire","J.K Rowling",100,true);
-let book2 = new Book("Dune","Harry Styles",699,false);
-let book3 = new Book("Revenge of the Man","Vengeful man",999,true);
 
-myLibrary.push(book1,book2,book3);
+if(!(getCurrentBooks())){ //initial population if the library hasn't been set yet
 
+    
+    let book1 = new Book("Harry Potter: Goblet of Fire","J.K Rowling",100,true);
+    let book2 = new Book("Dune","Harry Styles",699,false);
+    let book3 = new Book("Revenge of the Man","Vengeful man",999,true);
 
+    var libraryArray = [book1,book2,book3];
+
+    localStorage.setItem("library", JSON.stringify(libraryArray));
+
+}
 
 
 
